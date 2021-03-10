95811040ba8c2db7d4f155509372e0a6c02f98bf
Stella Vilar <vilar.stella@gmail.com>
2021-03-11 00:16:33 +0100

    Add item into list component thanks to input

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import {addTodo} from '../redux/actions';
+import { useDispatch } from 'react-redux';
 
 //Styles for material ui components
 const useStyles = makeStyles((theme) => ({
@@ -19,11 +21,25 @@ const useStyles = makeStyles((theme) => ({
 function InputComponent () {
     //Classes for material ui components
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    //input value
+    const [ item, setItem ] = useState('');
 
     return (
         <div className="input-part">
             <form
                 className={classes.form} 
+                onSubmit={e =>{ 
+                    e.preventDefault();
+                    dispatch(addTodo(
+                        {
+                            id: 34,
+                            name: item
+                        }
+                ))
+                setItem('');
+            }}
             >
                 <TextField
                     className={classes.input}
@@ -36,6 +52,8 @@ function InputComponent () {
                     name="todo"
                     autoComplete="todo"
                     autoFocus
+                    value={item}
+                    onChange={e => setItem(e.target.value)}
                 />
             </form>
         </div>
